a2f9f24cde146063ad460f1fb6bdc05a2c73e7c0
Pruthviraj Shinde <pruthviraj.shinde24@gmail.com>
2025-07-31 11:33:20 +0530

    branch selection file

---
+++ Artificial_Intelligence_Lab/subject1.py
@@ -0,0 +1,9 @@
+print("select any two subject :\n1.math\n2.physics\n3.programming.AI concept")
+sub1=int(input("enter the first subject number : "))
+sub=int(input("enter the second subject number : "))
+if sub1==1 and sub2==2 :
+             print("career sugested is computer engeneering :")
+if sub1==1 and sub2==1 :
+             print("carrer suggested ismechanical engeneering :")
+if sub1==1 and sub2==3 :
+             print("carrer suggested id AIDS engineering :")
